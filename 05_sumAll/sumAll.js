243d007ef2c2e0a77e496f7628201e62058e92b6
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-05-22 21:18:43 -0400

    Complete excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(numA, numB) {
+    if (typeof numA != 'number' || typeof numB != 'number' || numA < 0 || numB < 0) {
+        return 'ERROR';
+    } else if (numA <= numB) {
+        let arraySum = 0;
+        for (let i=numA; i<=numB; i++) {
+            arraySum += i;
+        }
+        return arraySum;
+    } else if (numA > numB) {
+        let arraySum = 0;
+        for (let i=numB; i<=numA; i++) {
+            arraySum += i;
+        }
+        return arraySum;
+    }
 };
 
 // Do not edit below this line
