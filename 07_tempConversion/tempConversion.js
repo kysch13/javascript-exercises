22b3e69813c3d014fdbb5ecdf73ffe3f1e0ce6d3
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-05-23 21:05:04 -0400

    Complete excercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  const cTemp = (fTemp - 32) * (5/9);
+  return (Math.round(cTemp * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  const fTemp = (cTemp * (9/5) + 32);
+  return (Math.round(fTemp * 10) / 10);
 };
 
 // Do not edit below this line
