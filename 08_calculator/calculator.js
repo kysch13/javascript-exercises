16051a83461fedd9bb563bcd2992572a9e43894a
unknown <124356972+kysch13@users.noreply.github.com>
2024-06-14 22:46:33 -0400

    Complete exercise 08_CALCULATOR

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumlulator, current) => accumlulator + current, 
+  0);
 };
 
-const multiply = function() {
-
+const multiply = function([...arr]) {
+  return arr.reduce((accum, current) => accum * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factProduct = 1;
+  if (a > 0) {
+    for (let i=1; i<=a; i++) {
+      factProduct = factProduct*i;
+    }
+  }
+  return factProduct;
 };
 
 // Do not edit below this line
