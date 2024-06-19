64687589a3467bb36b0ae6cf818a2d62c915596b
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-06-18 21:43:46 -0400

    Complete excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const cleanText = text.replace(/[^\w\s\']|_/g, '').replace(/\s+/g, '').toLowerCase();
+    let reverse = '';
+    for (let i = cleanText.length-1; i>=0; i--) {
+        reverse = reverse+cleanText[i];
+    }
+    return cleanText === reverse;
 };
+
+    console.log(palindromes('r3ace3car'));  
 
 // Do not edit below this line
 module.exports = palindromes;
