b0e5f5ae1ffd39713c937c0913506bde8261797e
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-07-14 21:52:22 -0400

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort(function (a, b) {
+        let ageA = 0;
+        let ageB = 0;
+        let currentYear = new Date().getFullYear();
+        if ('yearOfDeath' in a) {
+           ageA = a.yearOfDeath - a.yearOfBirth;
+      } else {
+          ageA = currentYear - a.yearOfBirth;
+      }
+      if ('yearOfDeath' in b) {
+           ageB = b.yearOfDeath - b.yearOfBirth;
+      } else {
+          ageB = currentYear - b.yearOfBirth;
+      }
+      if (ageA > ageB) return -1;
+      if (ageA < ageB) return 1;
+  });
+  return people[0];
 
 };
 
